1101698d1d85c3f7589be6bdb266e8e2de8e6c49
Martin Vogel <mvogel@steam.local>
2018-07-24 16:29:00 +0200

    Included arguments and a little help text
    Signed-off-by: Martin Vogel <mvogel@steam.local>

--- Printouts/QRCodes/wikicrawler.py
+++ Printouts/QRCodes/wikicrawler.py
@@ -2,98 +2,117 @@ import mwapi
 import json
 import requests
 import re
+import argparse
 from bs4 import BeautifulSoup
 session = mwapi.Session(host='https://wiki.comakingspace.de', api_path='/api.php')
+
+parser = argparse.ArgumentParser(description='Generate Tool printouts from the CoMakingSpace Wiki (https://wiki.comakingspace.de)')
+categoryGroup = parser.add_mutually_exclusive_group()
+categoryGroup.add_argument('--Machine', dest='MachineBox', action='store_true', 
+                    help='Crawl for Machine Info Boxes')
+categoryGroup.add_argument('--Tool', dest='ToolBox', action='store_true',
+                    help='Crawl for Tool Info Boxes')
+categoryGroup.add_argument('--Material', dest='MaterialBox', action='store_true',
+                    help='Crawl for Machine Info Boxes')
+categoryGroup.add_argument('--Project', dest='ProjectBox', action='store_true',
+                    help='Crawl for Project Info Boxes')
+categoryGroup.add_argument('--All', dest='All', action='store_true',
+                    help='Crawl for All Info Boxes')
+parser.add_argument('--Page', dest='Page', default='', help='Parse a specific page (use only with --InfoBoxName)')
+parser.add_argument('--InfoBoxName', dest='InfoBoxName', default='', help='Indicate the InfoBoxName when parsing a specific page (use only with --Page)')
+args = parser.parse_args()
+
 def crawlCategory(categoryname, infoboxname):
     global session
     print("-----------------------------------------------------")
-    print("Crawling Category " + categoryname)
-    
-    query_result = session.get(action='query', list='categorymembers',cmtitle='Category:' + categoryname)
+    print("Crawling Category " + categoryname)    
+    query_result = session.get(action='query', list='categorymembers', cmtitle='Category:' + categoryname)
     machine_pages = query_result['query']['categorymembers']
     for page in machine_pages:
         # get the content of the page
-        crawlpage(page['title'],infoboxname,categoryname)
-
+        crawlpage(page['title'], infoboxname, categoryname)
 def crawlBacklinks(backlinkpage, infoboxname):
     global session
     print("-----------------------------------------------------")
     print("Crawling Backlinks to: %s" % backlinkpage)
-    
-    query_result = session.get(action='query', list='backlinks',bltitle=backlinkpage,bllimit='max')
+    query_result = session.get(action='query', list='backlinks', bltitle=backlinkpage, bllimit='max')
     machine_pages = query_result['query']['backlinks']
-    machine_pages = sorted(machine_pages,key= lambda page: page['title'],reverse=False)
+    machine_pages = sorted(machine_pages, key=lambda page: page['title'], reverse=False)
     for page in machine_pages:
         # get the content of the page
-        crawlpage(page['title'],infoboxname,infoboxname)
-
-def crawlpage(title,infoboxname, categoryname = ''):
+        crawlpage(page['title'], infoboxname, infoboxname)
+def crawlpage(title, infoboxname, categoryname=''):
     print(title)
     rawurl = "https://wiki.comakingspace.de/index.php?title=" + title + '&action=raw'
     responseraw = requests.get(rawurl)
-    infoboxes = extractinfoboxes(responseraw.text,infoboxname)
+    infoboxes = extractinfoboxes(responseraw.text, infoboxname)
     number = 1
     for infobox in infoboxes:
         html = parseToolbox(infobox, title)
         basefilename = title.strip().replace(' ', '_')
         basefilename = re.sub(r'(?u)[^-\w.]', '', basefilename)
-        with open('%s_%s_%i.html' % (categoryname,basefilename,number),'w',encoding='utf8') as f:
+        with open('%s_%s_%i.html' % (categoryname, basefilename, number), 'w', encoding='utf8') as f:
             f.write(html.prettify())
             number += 1
-
-
-
 def parseToolbox(infoboxtext, title):
     global session
-    url= "https://wiki.comakingspace.de/" + title
-    parsingresponse = session.get(action='parse', text = infoboxtext, contentmodel = 'wikitext', disablelimitreport = 1)
+    url = "https://wiki.comakingspace.de/" + title
+    parsingresponse = session.get(action='parse', text=infoboxtext, contentmodel='wikitext', disablelimitreport=1)
     parsedwikitext = parsingresponse['parse']['text']['*']
     parsedwikitext = ('<html><body>' + parsedwikitext + '</body></html>')
-
-    html = BeautifulSoup(parsedwikitext,'html.parser')
-    image_link = html.find('a','image')
+    html = BeautifulSoup(parsedwikitext, 'html.parser')
+    image_link = html.find('a', 'image')
     image_tag = image_link.find('img')
     new_figure = html.new_tag('figure')
     Image_tag_copy = image_tag.replaceWith(new_figure)
     new_figure.append(Image_tag_copy)
     figurecaption = html.new_tag('figcaption')
-    figurecaption.string = 'Wiki: %s' % title
+    figuredescription = html.new_tag('small')
+    figuredescription.string = "For further information please scan the QR-Code or check our"
+    figurecaption.append(figuredescription)
+    figurecaption.append(html.new_tag('br'))
+    figurecaption.append('Wiki: %s' % title)
     new_figure.append(figurecaption)
     image_tag['src'] = 'http://chart.apis.google.com/chart?chs=200x200&cht=qr&chl=' + url
     del image_tag['srcset']
     image_tag['height'] = 200
     image_tag['width'] = 200
     table = html.find('table')
-    table['style'] = table['style'].replace('float:right;','')
+    table['style'] = table['style'].replace('float:right;', '')
     for a in html.findAll('a'):
         a.replaceWithChildren()
     return html
-
-def extractinfoboxes(wikitext,infoboxname):
+def extractinfoboxes(wikitext, infoboxname):
     infoboxes = []
-    searchstart = 0
-    number_of_infoboxes = wikitext.count("{{" + infoboxname)
     infoboxstart = wikitext.find("{{" + infoboxname)
     infoboxtext = None
     while infoboxstart != -1:
-        infoboxend = wikitext.find("}}",infoboxstart)
-        checkstart = infoboxstart
+        infoboxend = wikitext.find("}}", infoboxstart)
         infoboxtext = wikitext[infoboxstart:infoboxend+2]
         while infoboxtext.count("{{") != infoboxtext.count('}}'):
-            infoboxend = wikitext.find("}}",infoboxend+2)
+            infoboxend = wikitext.find("}}", infoboxend+2)
             infoboxtext = wikitext[infoboxstart:infoboxend+2]
         if infoboxtext is not None:
             infoboxes.append(infoboxtext)
-        infoboxstart = wikitext.find("{{" + infoboxname,infoboxend)
+        infoboxstart = wikitext.find("{{" + infoboxname, infoboxend)
     return infoboxes
 
 if __name__ == "__main__":
-    crawlBacklinks('Template:ToolInfoBox','ToolInfoBox')
-    crawlBacklinks('Template:MachineInfoBox','MachineInfoBox')
-    crawlBacklinks('Template:MaterialInfoBox','MaterialInfoBox')
-    crawlBacklinks('Template:ProjectInfoBox','ProjectInfoBox')
-    #crawlpage('Eccentric_Sanders','ToolInfoBox','Test')
-    #crawlCategory("Audio", "ProjectInfoBox")
-    #crawlCategory("Hardware", "ToolInfoBox")
-    #crawlCategory("Power Tools", "ToolInfoBox")
-    #crawlCategory("Machines", "MachineInfoBox")
+    if (args.MachineBox or args.All):
+        crawlBacklinks('Template:MachineInfoBox', 'MachineInfoBox')
+    if (args.MaterialBox or args.All):
+        crawlBacklinks('Template:MaterialInfoBox', 'MaterialInfoBox')
+    if (args.ProjectBox or args.All):
+        crawlBacklinks('Template:ProjectInfoBox', 'ProjectInfoBox')
+    if (args.ToolBox or args.All):
+        crawlBacklinks('Template:ToolInfoBox', 'ToolInfoBox')
+    if (args.Page!= ''):
+        if (args.InfoBoxName!=''):
+            crawlpage(args.Page, args.InfoBoxName)
+        else:
+            print('Please indicate the Info Box Name when crawling a specific page.')
+    # crawlpage('Eccentric_Sanders','ToolInfoBox','Test')
+    # crawlCategory("Audio", "ProjectInfoBox")
+    # crawlCategory("Hardware", "ToolInfoBox")
+    # crawlCategory("Power Tools", "ToolInfoBox")
+    # crawlCategory("Machines", "MachineInfoBox")
